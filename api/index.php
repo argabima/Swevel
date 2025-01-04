0ec0a3d73c22dcdede04be49174e3e31d62d11a7
Arga Bimantara <argabimantara8@gmail.com>
2025-01-04 21:01:57 +0700

    InitialProject
    Deployment in vercel - add component

---
+++ api/index.php
@@ -0,0 +1,10 @@
+<?php
+
+// Menyertakan autoload dari composer
+require __DIR__ . '/../vendor/autoload.php';
+
+// Menyertakan konfigurasi aplikasi CodeIgniter
+$path = __DIR__ . '/../public/index.php';
+
+// Jalankan aplikasi CI4
+include($path);
